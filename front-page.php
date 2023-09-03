ace5c4a0df825dd211c7aacb8aa056e22ba545fd
Haly Yampolska <halyna.yampolska@gmail.com>
2023-09-03 16:35:29 +0300

    Created front-page /css/

--- front-page.php
+++ front-page.php
@@ -3,10 +3,12 @@
     while(have_posts()) {
         the_post(); ?>
         <section class="acquaintance">
+            
+            <div class="hero">
+            <img class="hero__img" src="https://images.unsplash.com/photo-1628126907372-761f54441c1b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fG93bHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="">
             <div class="typing">
                 <h1 class="acquaintance__name">I glad to see you here!</h1>
-            </div>
-            <p class="acquaintance__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                <p class="acquaintance__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                 Ut enim ad minim veniam, quis nostrud exercitation ullamco 
                 laboris nisi ut aliquip ex ea commodo consequat. 
@@ -14,6 +16,9 @@
                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
                 cupidatat non proident, sunt in culpa qui officia deserunt mollit 
                 anim id est laborum.</p>
+            </div>
+            
+            </div>
             <div class="button__standart">    
                 <button class="btn__standart">More about me</button>
                 <button class="btn__standart">Go to the Portfolio</button>
@@ -24,19 +29,26 @@
             <h2 class="skills__name">Skills</h2>
             <div class="skill">
                 <ul class="skill__list">
-                    <li>PHP</li>
-                    <li>WordPress</li>
-                    <li>CSS/SCSS</li>
-                    <li>HTML</li>
-                    <li>JavaScript</li>
-                    <li>GitHub</li>
+                    <div class="row-1">
+                    <li><a href="">PHP</a></li>
+                    <li><a href="">WordPress</a></li>
+                    <li><a href="">CSS/SCSS</a></li>
+                    <li><a href="">HTML</a></li>
+                    </div>
+                    <div class="row-2">
+                    <li><a href="">JavaScript</a></li>
+                    <li><a href="">GitHub</a></li>
+                    <li><a href="">REST API</a></li>
+                    </div>
                 </ul>
             </div>
         </section>
-
-        <section class="projects">
-            <h2 class="projects__name">My Recent Projects</h2>
+        
+        <h2 class="projects__name">My Recent Projects</h2>
+        
+        <section class="projects">    
             <div class="project">
+                <img class="project__photo" src="https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="">
                 <h3 class="project__name">Project 1</h3>
                 <p class="project__type">Type: <a href="#">Website</a></p>
                 <p class="project__stack">Stack: <a href="#">HTML, CSS & SCSS, JavaScript, iQuery, npm, PHP, WordPress</a></p>
@@ -50,13 +62,47 @@
                     cupidatat non proident, sunt in culpa qui officia deserunt mollit 
                     anim id est laborum.
                 </p>
-                <button class="btn__project">See More</button>
+                <button class="btn__project">See More Details</button>
+            </div>
+            <div class="project">
+                <img class="project__photo" src="https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="">
+                <h3 class="project__name">Project 2</h3>
+                <p class="project__type">Type: <a href="#">Website</a></p>
+                <p class="project__stack">Stack: <a href="#">HTML, CSS & SCSS, JavaScript, iQuery, npm, PHP, WordPress</a></p>
+                <p class="project__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse 
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                    anim id est laborum.
+                </p>
+                <button class="btn__project">See More Details</button>
+            </div>
+            <div class="project">
+                <img class="project__photo" src="https://images.unsplash.com/photo-1483058712412-4245e9b90334?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="">
+                <h3 class="project__name">Project 3</h3>
+                <p class="project__type">Type: <a href="#">Website</a></p>
+                <p class="project__stack">Stack: <a href="#">HTML, CSS & SCSS, JavaScript, iQuery, npm, PHP, WordPress</a></p>
+                <p class="project__text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco 
+                    laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse 
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat 
+                    cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                    anim id est laborum.
+                </p>
+                <button class="btn__project">See More Details</button>
             </div>
         </section>
 
         <section class="invite">
             <h2 class="invite__name">Let's chat!</h2>
-            <div class="invite__button">
+            <div class="invite__buttons">
                 <button class="btn__invite">Download CV</button>
                 <button class="btn__invite">Get in Touch</button>
             </div>
--- header.php
+++ header.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
+
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
 </head>
 <body>
     <header class="header">
-        <div class="header__name-menu"><a href="#">Halyna Yampolska</a></div>
+        <div class="header__name-menu"><a href="<?php echo site_url(); ?>">Halyna Yampolska</a></div>
         <div class="header__menu">
             <ul>
-                <li><a href="">Home</a></li>
+                <li><a href="<?php echo site_url(); ?>">Home</a></li>
                 <li><a href="">About</a></li>
                 <li><a href="">Portfolio</a></li>
                 <li><a href="">Contact</a></li>
